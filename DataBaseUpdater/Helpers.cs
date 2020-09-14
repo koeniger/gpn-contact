8aca2a229baf303ab9aeedb893e6b0858ef9dece
Елизавета Погудо <pogudo96@mail.ru>
2020-09-14 16:01:39 +0200

    added user init in DataBaseUpdater

--- DataBaseUpdater/Helpers.cs
+++ DataBaseUpdater/Helpers.cs
@@ -12,5 +12,15 @@ namespace DataBaseUpdater
                 .AddJsonFile(StringResources.AppSettingsFileName);
             return configBuilder.Build();
         }
+
+        public static string AddDbRolesScript()
+        {
+            return
+                "do $do$ begin IF NOT EXISTS( " +
+                "SELECT FROM pg_catalog.pg_roles WHERE rolname = 'user_db') THEN " +
+                "CREATE USER user_db WITH LOGIN PASSWORD 'gfer'; " +
+                "ALTER USER user_db CREATEDB; " +
+                "END IF; END $do$;";
+        }
     }
 }
